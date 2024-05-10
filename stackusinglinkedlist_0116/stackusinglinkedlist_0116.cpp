1bf6aa7b91932a28b0ce7a07c8d39773b0d692b4
ZahwaAnritia <anritia15@gmail.com>
2024-05-10 09:27:47 +0700

    menambahkan implementasi fungsi pop dan isEmpty di dalam kelas Stack

--- stackusinglinkedlist_0116/stackusinglinkedlist_0116.cpp
+++ stackusinglinkedlist_0116/stackusinglinkedlist_0116.cpp
@@ -28,6 +28,17 @@ public:
 		return value;
 	}
 
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value:" << top->data << endl;
+		top = top->next;
+	}
+	bool isEmpty() {
+		return top == NULL;
+	}
+
 
 	
 };
